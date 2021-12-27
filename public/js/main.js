a7617899f0dc348ab0a4b10f1be8d7bbbd5f0c72
netovieira01_ <76048368+manoelvieira0@users.noreply.github.com>
2021-12-27 07:12:34 -0300

    Primeiros passos com jQuery

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,5 @@
-console.log('Olá')
+var frase = $('.frase').text();
+var numPalavras = frase.split(" ").length; //Split => Divide a String em espaços.
+var tamanhoFrase = $('#tamanho-frase')
+
+tamanhoFrase.text(numPalavras)
