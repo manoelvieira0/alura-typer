3663ed551dcfacab6dc3c362bbfc2dd2ba0dac67
netovieira01_ <76048368+manoelvieira0@users.noreply.github.com>
2021-12-27 09:10:54 -0300

    Reiniciando o jogo e organizando o código em funções

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,47 @@
-var frase = $('.frase').text();
-var numPalavras = frase.split(" ").length; //Split => Divide a String em espaços.
-var tamanhoFrase = $('#tamanho-frase')
-tamanhoFrase.text(numPalavras)
-
+var tempoInicial = $('#tempo-digitacao').text()
 var campo = $('.campo-digitacao')
-campo.on('input', function(){ //Input atualiza enquanto o usuário seleciona o campo
-    var conteudo = campo.val()
 
-    var qtdPalavras = conteudo.split(/\S+/).length -1
-    $('#contador-palavras').text(qtdPalavras)
-    
-    var qtdCaracteres = conteudo.length
-    $('#contador-caracteres').text(qtdCaracteres)
-})
+$(
+    function () {
+        console.log('Olá')
+        inicializaContadores()
+        inicializaCronometro()
+        $("#botao-reiniciar").click(reiniciaJogo)
+    })
 
-var tempoRestante = $('#tempo-digitacao').text()
-campo.one('focus', function(){
-    var cronometroID = setInterval(() => {
-        tempoRestante--;
-        $('#tempo-digitacao').text(tempoRestante)
-        if(tempoRestante < 1){
-            campo.attr('disabled', true)
-            clearInterval(cronometroID)
-        }
-    }, 1000);
-})
+function inicializaContadores() {
+
+    campo.on('input', function () { //Input atualiza enquanto o usuário seleciona o campo
+        var conteudo = campo.val()
+
+        var qtdPalavras = conteudo.split(/\S+/).length - 1
+        $('#contador-palavras').text(qtdPalavras)
+
+        var qtdCaracteres = conteudo.length
+        $('#contador-caracteres').text(qtdCaracteres)
+    })
+}
+
+function inicializaCronometro() {
+    var tempoRestante = $('#tempo-digitacao').text()
+    campo.one('focus', function () {
+        var cronometroID = setInterval(() => {
+            tempoRestante--;
+            $('#tempo-digitacao').text(tempoRestante)
+            if (tempoRestante < 1) {
+                campo.attr('disabled', true)
+                clearInterval(cronometroID)
+            }
+        }, 1000);
+    })
+}
+
+function reiniciaJogo() {
+    campo.attr('disabled', false)
+    campo.val('')
+    $('#contador-palavras').text('0')
+    $('#contador-caracteres').text('0')
+    $("#tempo-digitacao").text(tempoInicial)
+    inicializaCronometro()
+}
+
