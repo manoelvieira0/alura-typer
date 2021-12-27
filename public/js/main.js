709c0b5c5391f66dd639c21acaea3a2218370bf7
netovieira01_ <76048368+manoelvieira0@users.noreply.github.com>
2021-12-27 07:33:16 -0300

    Selecionando Eventos com JQuery

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,15 @@
 var frase = $('.frase').text();
 var numPalavras = frase.split(" ").length; //Split => Divide a String em espaços.
 var tamanhoFrase = $('#tamanho-frase')
-
 tamanhoFrase.text(numPalavras)
+
+var campo = $('.campo-digitacao')
+campo.on('input', function(){ //Input atualiza enquanto o usuário seleciona o campo
+    var conteudo = campo.val()
+
+    var qtdPalavras = conteudo.split(/\S+/).length -1
+    $('#contador-palavras').text(qtdPalavras)
+    
+    var qtdCaracteres = conteudo.length
+    $('#contador-caracteres').text(qtdCaracteres)
+})
